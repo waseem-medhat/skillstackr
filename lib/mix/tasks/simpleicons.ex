080a3d646ab7bbae96c88473e802e73c541f1c06
waseem-medhat <wipdev.tech@gmail.com>
2024-06-07 19:57:38 +0300

    Convert simpleicons script into a Mix task

--- lib/mix/tasks/simpleicons.ex
+++ lib/mix/tasks/simpleicons.ex
@@ -1,11 +1,13 @@
-defmodule Skillstackr.Utils.SimpleIcons do
+defmodule Mix.Tasks.Simpleicons do
   @moduledoc """
-  Functions used to pull and parse data from the SimpleIcons repository in
-  order to create a JSON file with the icon data and SVGs.
+  Pulls and parses the data from the SimpleIcons repository in order to create
+  a JSON file with the icon data and SVGs.
   """
+  use Mix.Task
 
-  def download_icons!(download_dir, repo_url) do
+  defp download_icons!(download_dir, repo_url) do
     if not File.exists?(download_dir) do
+      IO.puts("Icon repo clone not found, cloning...")
       case System.cmd("git", ["clone", repo_url, download_dir]) do
         {_, 0} -> :ok
         _ -> raise("error cloning")
@@ -26,18 +28,23 @@ defmodule Skillstackr.Utils.SimpleIcons do
     |> put_in([:svg_map, name], file)
   end
 
-  def parse_files() do
+  defp parse_files() do
     File.ls!()
     |> Enum.reduce(%{translation_map: %{}, svg_map: %{}}, &build_map_entry/2)
   end
+
+  def run([]) do
+    assets_path = Path.join(["priv", "static", "assets"])
+
+    json =
+      Path.join(assets_path, "simple-icons")
+      |> download_icons!("https://github.com/simple-icons/simple-icons.git")
+      |> File.cd!(&parse_files/0)
+      |> Jason.encode!()
+
+    Path.join(assets_path, "simpleicons_map.json")
+    |> File.write!(json)
+
+    IO.puts("Done!")
+  end
 end
-
-alias Skillstackr.Utils.SimpleIcons
-
-json =
-  Path.join(["priv", "utils", "simple-icons"])
-  |> SimpleIcons.download_icons!("https://github.com/simple-icons/simple-icons.git")
-  |> File.cd!(&SimpleIcons.parse_files/0)
-  |> Jason.encode!()
-
-File.write!(Path.join(["priv", "utils", "simpleicons_map.json"]), json)
