a39f2caa75636f5d29c4b1d7649c78750b88c82d
waseem-medhat <wipdev.tech@gmail.com>
2024-06-09 17:31:16 +0300

    Get new profile template + change title rendering

--- lib/skillstackr_web/controllers/page_controller.ex
+++ lib/skillstackr_web/controllers/page_controller.ex
@@ -3,6 +3,7 @@ defmodule SkillstackrWeb.PageController do
 
   def home(conn, _params) do
     conn
+    |> assign(:page_title, "Welcome")
     |> render(:home, layout: false)
   end
 end
--- lib/skillstackr_web/live/new_profile_live/new_profile_live.ex
+++ lib/skillstackr_web/live/new_profile_live/new_profile_live.ex
@@ -1,13 +1,15 @@
 defmodule SkillstackrWeb.NewProfileLive do
+  alias Skillstackr.Profiles
+  alias Skillstackr.Profiles.Profile
   use SkillstackrWeb, :live_view
 
-  def render(assigns) do
-    ~H"""
-    Hello new profile
-    """
-  end
-
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:page_title, "New Profile")
+      |> assign(:changeset, Profiles.change_profile(%Profile{}))
+      |> assign(:technologies, TechnologyComponents.get_names())
+
     {:ok, socket}
   end
 end
