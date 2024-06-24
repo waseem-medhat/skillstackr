dc2b290fea1eff0f4e7724e80532c10c815d156e
waseem-medhat <wipdev.tech@gmail.com>
2024-06-24 23:14:23 +0300

    Fetch and roughly display jobs in listing page

--- lib/skillstackr_web/live/jobs_live/jobs_live.ex
+++ lib/skillstackr_web/live/jobs_live/jobs_live.ex
@@ -1,9 +1,11 @@
 defmodule SkillstackrWeb.JobsLive do
+  alias Skillstackr.Jobs
   use SkillstackrWeb, :live_view
 
   def mount(_params, _session, socket) do
     socket = 
       socket
+      |> assign(:jobs, Jobs.list_jobs())
       |> assign(:page_title, "Job Experience")
 
     {:ok, socket}
