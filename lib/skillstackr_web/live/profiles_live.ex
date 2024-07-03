01e6efc33b98fb7a478b44873d0a2fd108479d69
waseem-medhat <wipdev.tech@gmail.com>
2024-07-03 23:25:57 +0300

    Improve design of profile listing

--- lib/skillstackr_web/live/profiles_live.ex
+++ lib/skillstackr_web/live/profiles_live.ex
@@ -34,44 +34,48 @@ defmodule SkillstackrWeb.ProfilesLive do
       <i>No profiles</i>
     </div>
 
-    <div
-      :for={p <- @profiles}
-      class="relative bg-white dark:bg-slate-950/70 rounded-lg my-5 shadow-lg p-5 transition"
-    >
-      <hgroup class="flex-grow mb-2">
-        <h1 class="text-2xl font-bold">
-          <%= p.full_name %>
-          <.link class="font-medium text-lg ms-1 text-neutral-500"><%= p.slug %></.link>
-        </h1>
-        <h2 class="text-lg font-semibold"><%= p.headline %></h2>
-      </hgroup>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5 my-5">
+      <div
+        :for={p <- @profiles}
+        class="relative bg-white dark:bg-slate-950/70 rounded-lg shadow-lg p-6 transition flex flex-col"
+      >
+        <hgroup class="mb-2">
+          <h2 class="text-2xl font-bold">
+            <%= p.full_name %>
+            <.link class="font-medium text-base ms-1 text-neutral-500"><%= p.slug %></.link>
+          </h2>
+          <h3 class="text-lg font-semibold"><%= p.headline %></h3>
+        </hgroup>
 
-      <p><%= p.summary || "No summary" %></p>
+        <p class="text-sm text-gray-500 dark:text-neutral-400 flex-grow">
+          <%= p.summary || "No summary" %>
+        </p>
 
-      <section class="mt-5">
-        <div class="flex gap-2">
-          <TechnologyComponents.tech_badge
-            :for={t <- p.profiles_technologies}
-            tech={t.technology.name}
-            size={18}
-          />
+        <section class="mt-5">
+          <div class="flex gap-2">
+            <TechnologyComponents.tech_badge
+              :for={t <- p.profiles_technologies}
+              tech={t.technology.name}
+              size={18}
+            />
+          </div>
+        </section>
+
+        <div class="absolute top-6 right-6 flex gap-2 items-center">
+          <.link
+            navigate={~p"/profiles/#{p.slug}"}
+            class="bg-indigo-700 text-white rounded-lg p-1.5 inline-flex items-center hover:bg-indigo-500 z-10 text-sm gap-1"
+          >
+            <.icon name="hero-eye-micro" />
+          </.link>
+
+          <.link
+            navigate={~p"/profiles/#{p.slug}/edit"}
+            class="bg-indigo-700 text-white rounded-lg p-1.5 inline-flex items-center hover:bg-indigo-500 z-10 text-sm gap-1"
+          >
+            <.icon name="hero-pencil-square-micro" />
+          </.link>
         </div>
-      </section>
-
-      <div class="absolute top-5 right-5 flex gap-2 items-center">
-        <.link
-          navigate={~p"/profiles/#{p.slug}"}
-          class="bg-indigo-700 text-white rounded-lg p-1.5 inline-flex items-center hover:bg-indigo-500 z-10 text-sm gap-1"
-        >
-          <.icon name="hero-eye-micro" />
-        </.link>
-
-        <.link
-          navigate={~p"/profiles/#{p.slug}/edit"}
-          class="bg-indigo-700 text-white rounded-lg p-1.5 inline-flex items-center hover:bg-indigo-500 z-10 text-sm gap-1"
-        >
-          <.icon name="hero-pencil-square-micro" />
-        </.link>
       </div>
     </div>
     """
