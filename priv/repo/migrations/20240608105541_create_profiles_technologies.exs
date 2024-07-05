c99b5c35464eb92394a1a4597a4fb38837ec241b
waseem-medhat <wipdev.tech@gmail.com>
2024-07-05 20:25:33 +0300

    Fix migrations for many-to-many tables

--- priv/repo/migrations/20240608105541_create_profiles_technologies.exs
+++ priv/repo/migrations/20240608105541_create_profiles_technologies.exs
@@ -2,9 +2,15 @@ defmodule Skillstackr.Repo.Migrations.CreateProfilesTechnologies do
   use Ecto.Migration
 
   def change do
-    create table(:profiles_technologies) do
-      add :profile_id, references(:profiles, type: :binary_id, on_delete: :delete_all)
-      add :technology_id, references(:technologies, type: :binary_id, on_delete: :delete_all)
+    create table(:profiles_technologies, primary_key: false) do
+      add :profile_id,
+          references(:profiles, type: :binary_id, on_delete: :delete_all),
+          primary_key: true
+
+      add :technology_id,
+          references(:technologies, type: :binary_id, on_delete: :delete_all),
+          primary_key: true
+
       timestamps(type: :utc_datetime, default: fragment("(CURRENT_TIMESTAMP)"))
     end
   end
