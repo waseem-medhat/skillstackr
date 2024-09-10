41994a323d0ec5b927556be290588b423d3d6ec4
waseem-medhat <wipdev.tech@gmail.com>
2024-09-10 13:56:50 +0300

    Add image preview to UI

--- lib/skillstackr_web/live/profile_form_live.ex
+++ lib/skillstackr_web/live/profile_form_live.ex
@@ -183,18 +183,24 @@ defmodule SkillstackrWeb.ProfileFormLive do
         <.input field={@form[:headline]} type="text" label="Headline" />
         <.input field={@form[:email]} type="email" label="Email" />
         <div class="flex gap-3">
-          <div class="w-full">
+          <div class="flex-grow">
             <.label>Profile Photo</.label>
             <.live_file_input
               upload={@uploads.profile_photo}
               class="mt-2 block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-primary focus:ring-primary disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 file:bg-gray-50 file:border-0 file:me-4 file:py-3 file:px-4 dark:file:bg-neutral-700 dark:file:text-neutral-400"
             />
           </div>
-          <img
-            class="inline-block size-20 aspect-square rounded-full contrast-0"
-            src={~p"/images/profile-icon.png"}
-            alt="Image Description"
-          />
+          <div class="size-20">
+            <%= if length(@uploads.profile_photo.entries) > 0 do %>
+              <.live_img_preview entry={hd(@uploads.profile_photo.entries)} class="rounded-full" />
+            <% else %>
+              <img
+                class="inline-block aspect-square rounded-full contrast-0"
+                src={~p"/images/profile-icon.png"}
+                alt="Image Description"
+              />
+            <% end %>
+          </div>
         </div>
         <.input field={@form[:summary]} type="textarea" label="Professional Summary" />
       </section>
