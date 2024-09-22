995e831c81bdfcd1d6f67566fb91cda87655c027
waseem-medhat <wipdev.tech@gmail.com>
2024-09-23 01:46:36 +0300

    Remove CRUD tests
    All functions so far has been implementing CRUD operations by calling
    Ecto functions directly. We don't need to test that "Ecto works."

--- test/skillstackr/jobs_test.exs
+++ test/skillstackr/jobs_test.exs
@@ -4,76 +4,13 @@ defmodule Skillstackr.JobsTest do
   alias Skillstackr.Jobs
 
   describe "jobs" do
-    alias Skillstackr.Jobs.Job
-    import Skillstackr.{JobsFixtures, AccountsFixtures, ProfilesFixtures}
-
-    @invalid_attrs %{experience_years: -1}
+    import Skillstackr.{JobsFixtures, AccountsFixtures}
 
     @valid_attrs %{
       title: "Senior Bikeshedder",
       company: "The Bikeshedding Company",
       experience_years: 4
     }
-
-    defp valid_profile_fixture(account_id) do
-      profile_fixture(%{account_id: account_id, full_name: "John Doe", slug: "johndoe"})
-    end
-
-    test "get_job/1 returns the job with given id" do
-      account = account_fixture()
-      job = @valid_attrs |> Map.put(:account_id, account.id) |> job_fixture()
-
-      assert Jobs.get_job(job.id) == job
-    end
-
-    test "create_job/1 with valid data creates a job" do
-      account = account_fixture()
-      attrs = Map.put(@valid_attrs, :account_id, account.id)
-
-      assert {:ok, %{job: %Job{}}} = Jobs.create_job(attrs)
-    end
-
-    test "create_job/1 associates profiles with a created job" do
-      %{id: account_id} = account_fixture()
-      profile_id = valid_profile_fixture(account_id).id
-
-      attrs = Map.put(@valid_attrs, :account_id, account_id)
-      {:ok, %{job: job}} = Jobs.create_job(attrs, [profile_id])
-
-      assoc_profile_ids =
-        Jobs.get_job(job.id).profiles_jobs
-        |> Enum.map(fn pj -> pj.profile.id end)
-
-      assert assoc_profile_ids == [profile_id]
-    end
-
-    test "create_job/1 with invalid data returns error changeset" do
-      assert {:error, :job, %Ecto.Changeset{}, %{}} = Jobs.create_job(@invalid_attrs)
-    end
-
-    test "update_job/4 with valid data updates the job" do
-      account = account_fixture()
-      job = @valid_attrs |> Map.put(:account_id, account.id) |> job_fixture()
-      update_attrs = %{}
-
-      assert {:ok, %{updated_job: %Job{}}} = Jobs.update_job(job, update_attrs)
-    end
-
-    test "update_job/4 with invalid data returns error changeset" do
-      account = account_fixture()
-      job = @valid_attrs |> Map.put(:account_id, account.id) |> job_fixture()
-
-      assert {:error, :updated_job, %Ecto.Changeset{}, %{}} = Jobs.update_job(job, @invalid_attrs)
-      assert job == Jobs.get_job(job.id)
-    end
-
-    test "delete_job/1 deletes the job" do
-      account = account_fixture()
-      job = @valid_attrs |> Map.put(:account_id, account.id) |> job_fixture()
-
-      assert {:ok, %Job{}} = Jobs.delete_job(job)
-      assert is_nil(Jobs.get_job(job.id))
-    end
 
     test "change_job/1 returns a job changeset" do
       account = account_fixture()
--- test/skillstackr/technologies_test.exs
+++ test/skillstackr/technologies_test.exs
@@ -1,27 +1,3 @@
 defmodule Skillstackr.TechnologiesTest do
   use Skillstackr.DataCase
-
-  alias Skillstackr.Technologies
-
-  describe "technologies" do
-    alias Skillstackr.Technologies.Technology
-
-    import Skillstackr.TechnologiesFixtures
-
-    @invalid_attrs %{name: ""}
-    @valid_attrs %{name: "React", category: "frontend"}
-
-    test "create_technology/1 with valid data creates a technology" do
-      assert {:ok, %Technology{}} = Technologies.create_technology(@valid_attrs)
-    end
-
-    test "create_technology/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Technologies.create_technology(@invalid_attrs)
-    end
-
-    test "change_technology/1 returns a technology changeset" do
-      technology = technology_fixture(@valid_attrs)
-      assert %Ecto.Changeset{} = Technologies.change_technology(technology)
-    end
-  end
 end
